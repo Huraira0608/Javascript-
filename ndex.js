46c69386d2909bff226e0e0ac288be50a693244f
portfolio web <160927199+Huraira0608@users.noreply.github.com>
2024-03-10 22:47:04 +0500

    done

---
+++ ndex.js
@@ -0,0 +1,10 @@
+// Function to print the 19's table
+function printNineteenTable() {
+    console.log("Multiplication Table of 19:");
+    for (let i = 1; i <= 10; i++) {
+        console.log("19 * " + i + " = " + (19 * i));
+    }
+}
+
+// Calling the function to print the table
+printNineteenTable();
